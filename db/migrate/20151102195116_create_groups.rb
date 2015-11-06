5aad994d56247f9dcb850bbe34aef8eef7e0b256
Alexander Juneja <alexjuneja1@gmail.com>
2015-11-06 10:16:32 -0800

    migration test fix

--- db/migrate/20151102195116_create_groups.rb
+++ db/migrate/20151102195116_create_groups.rb
@@ -4,7 +4,7 @@ class CreateGroups < ActiveRecord::Migration
       t.string :topic
       t.date :date
       t.string :location
-      t.references :userID, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
